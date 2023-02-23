483567223c79baededc7c36e11f59394053e1179
hydra <titusbaeiku05@gmail.com>
2023-02-23 03:59:55 +0000

    update for live server

--- index.js
+++ index.js
@@ -274,7 +274,7 @@ app.delete('/images/:filename', (req, res) => {
 
 function generateRandomString() {
     let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = '0123456789';
     const charactersLength = characters.length;
     for (let i = 0; i < 4; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
